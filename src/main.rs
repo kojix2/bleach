0e52a1c525bb14d9cd8ef693a62bcf3d5fc4fcda
kojix2 <2xijok@gmail.com>
2023-12-29 10:12:47 +0900

    Add support for cleaning ANSI color and movement sequences

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,16 @@ struct Opts {
     in_place: bool,
     #[clap(short, long)]
     backup: bool,
+    #[clap(short, long)]
+    clean_types: Option<Vec<String>>,
     #[clap(name = "FILE")]
     file: Option<String>,
 }
 
 fn main() -> io::Result<()> {
     let opt = Opts::parse();
-
     let mut buffer = String::new();
 
-    // Input file specified or read from stdin
     match &opt.file {
         Some(file_name) => {
             fs::File::open(file_name)?.read_to_string(&mut buffer)?;
@@ -29,10 +29,25 @@ fn main() -> io::Result<()> {
         }
     };
 
-    let re = Regex::new("\x1b\\[[0-9;]*m").unwrap();
-    let result = re.replace_all(&buffer, "");
+    // Different types of regex for different types of ANSI sequences
+    let color_re = Regex::new("\x1b\\[[0-9;]*m").unwrap();
+    let movement_re = Regex::new("\x1b\\[[0-9;]*[ABCD]").unwrap();
 
-    // In-place edit
+    let mut result = buffer.clone();
+
+    if let Some(types) = &opt.clean_types {
+        if types.contains(&"color".to_string()) {
+            result = color_re.replace_all(&result, "").into_owned();
+        }
+        if types.contains(&"movement".to_string()) {
+            result = movement_re.replace_all(&result, "").into_owned();
+        }
+        if types.contains(&"all".to_string()) {
+            result = color_re.replace_all(&result, "").into_owned();
+            result = movement_re.replace_all(&result, "").into_owned();
+        }
+    }
+
     if opt.in_place {
         if let Some(file_name) = &opt.file {
             if opt.backup {
